google-research/simclr

commit 180b56a9af72725c3ea694556782aac036f2935b

In LARS optimizer, do not increment global_step variable if not passed in. This doesn't affect existing code/result.

lars_optimizer.py
@@ -81,10 +81,6 @@ class LARSOptimizer(tf.train.Optimizer):
       self.exclude_from_layer_adaptation = exclude_from_weight_decay
 
   def apply_gradients(self, grads_and_vars, global_step=None, name=None):
-    if global_step is None:
-      global_step = tf.train.get_or_create_global_step()
-    new_global_step = global_step + 1
-
     assignments = []
     for (grad, param) in grads_and_vars:
       if grad is None or param is None:
@@ -139,10 +135,11 @@ class LARSOptimizer(tf.train.Optimizer):
         scaled_lr = trust_ratio * self.learning_rate
         next_param = param - scaled_lr * update
 
-      assignments.extend(
-          [param.assign(next_param),
-           v.assign(next_v),
-           global_step.assign(new_global_step)])
+      assignments.extend([param.assign(next_param), v.assign(next_v)])
+
+      if global_step is not None:
+        new_global_step = global_step + 1
+        assignments.append(global_step.assign(new_global_step))
     return tf.group(*assignments, name=name)
 
   def _use_weight_decay(self, param_name):

commit 036ea7baa3eeb255c88a08392fcdc1d55bb8133f

Update lars_optimizer.py
Fixing a small bug of global_step being updated more than once per actual training step (as the increment is applied during each grad update of a unique trainable variable).

This bug was accidentally introduced in https://github.com/google-research/simclr/commit/180b56a9af72725c3ea694556782aac036f2935b, and only affects the optimizer when global_step is passed into the optimizer.

lars_optimizer.py
@@ -137,9 +137,9 @@ class LARSOptimizer(tf.train.Optimizer):
 
       assignments.extend([param.assign(next_param), v.assign(next_v)])
 
-      if global_step is not None:
-        new_global_step = global_step + 1
-        assignments.append(global_step.assign(new_global_step))
+    if global_step is not None:
+      new_global_step = global_step + 1
+      assignments.append(global_step.assign(new_global_step))
     return tf.group(*assignments, name=name)
 
   def _use_weight_decay(self, param_name):