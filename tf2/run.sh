dafe86c39d6bd6cac13bb56cccca06dbaf5aae31
Nick Stracke <nick.stracke@web.de>
2022-02-09 11:57:00 +0100

    update train command

--- tf2/run.sh
+++ tf2/run.sh
@@ -1,4 +1,4 @@
-python3 run.py --dataset=pill --eval_split=train --use_tpu=False --model_dir=logs --train_mode=pretrain
+python3 run.py --dataset=pill --eval_split=train --use_tpu=False --model_dir=logs --train_mode=pretrain --image_size=800 --train_batch_size=16
 
 # --train_mode= pretrain or finetune
 # it's (obviously) necessary to pretrain first because the models loads a checkpoint.
