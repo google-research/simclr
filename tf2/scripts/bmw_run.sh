14cec9c50c257ed2175a6ec881ad2105ddb6daae
Nick Stracke <nick.stracke@web.de>
2022-02-27 14:48:35 +0530

    update run scripts

---
+++ tf2/scripts/bmw_run.sh
@@ -0,0 +1,19 @@
+python3 run.py --dataset=bmw --data_dir="/home/nick/bmw_data" --eval_split=test \
+--use_tpu=False --model_dir=logs --mode=train --train_mode=pretrain --image_size=224 \
+--image_size=224 --train_batch_size=128 --lineareval_while_pretraining=False
+
+# --train_mode= pretrain or finetune
+# it's (obviously) necessary to pretrain first because the models loads a checkpoint.
+
+# I guess for our project we would only pretrain the model
+
+# we then need to think about how to adapt the task for anomaly detection
+# instead of a projection head, I guess we could use Conv2DTranspose to restore the original image
+# and then compute a reconstruction loss over input and output
+
+
+# steps
+# import dataset to be class of TFDS builder
+# pretrain with that model
+# see what happens
+# add upscaling head and compute reconstruction loss (finetune)
--- tf2/scripts/mvtech_run.sh
+++ tf2/scripts/mvtech_run.sh
@@ -1,4 +1,8 @@
-python3 run.py --dataset=bmw --data_dir="/home/nick/bmw_data" --eval_split=train --use_tpu=False --model_dir=logs --mode=train --train_mode=pretrain --image_size=224 --image_size=224 --train_batch_size=128 --lineareval_while_pretraining=False
+python3 run.py --dataset=mvtech --data_dir="/home/nick/data" --eval_split=test \
+--use_tpu=False --model_dir=logs --mode=train --train_mode=train_then_eval --image_size=224 \
+--image_size=224 --train_batch_size=128 --lineareval_while_pretraining=False --train_test_ratio=0.3 \
+--eval_batch_size=128 --load_existing_split=False --min_fraction_anomalies=0.15 --run_id=001
+
 
 # --train_mode= pretrain or finetune
 # it's (obviously) necessary to pretrain first because the models loads a checkpoint.
